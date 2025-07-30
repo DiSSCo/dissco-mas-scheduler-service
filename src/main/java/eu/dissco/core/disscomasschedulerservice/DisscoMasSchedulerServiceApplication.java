540e42b83a79b41419cff2be201d1f2949e81d85
Soulaine <soulaine.theo@gmail.com>
2025-07-30 13:17:26 +0200

    sonar init

--- src/main/java/eu/dissco/core/disscomasschedulerservice/DisscoMasSchedulerServiceApplication.java
+++ src/main/java/eu/dissco/core/disscomasschedulerservice/DisscoMasSchedulerServiceApplication.java
@@ -1,4 +1,4 @@
-package eu.dissco.disscomasschedulerservice;
+package eu.dissco.core.disscomasschedulerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
