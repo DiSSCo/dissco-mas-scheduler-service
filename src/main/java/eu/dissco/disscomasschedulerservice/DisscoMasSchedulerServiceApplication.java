afa1da7033d163643802687fdf38d3b93b45fe7c
Soulaine <soulaine.theo@gmail.com>
2025-07-30 11:43:33 +0200

    Initial commit

---
+++ src/main/java/eu/dissco/disscomasschedulerservice/DisscoMasSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.dissco.disscomasschedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisscoMasSchedulerServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DisscoMasSchedulerServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/eu/dissco/disscomasschedulerservice/DisscoMasSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.dissco.disscomasschedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisscoMasSchedulerServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
