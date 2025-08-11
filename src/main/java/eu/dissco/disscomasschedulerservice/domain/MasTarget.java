cd344886f6099aed4f221a2bb3bccab0dae0e80f
Soulaine <soulaine.theo@gmail.com>
2025-08-11 13:38:29 +0200

    Target verification

--- src/main/java/eu/dissco/disscomasschedulerservice/domain/MasTarget.java
+++ src/main/java/eu/dissco/disscomasschedulerservice/domain/MasTarget.java
@@ -1,11 +1,9 @@
 package eu.dissco.disscomasschedulerservice.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public record MasTarget(
-    @JsonProperty("object")
-    JsonNode targetObject,
+    JsonNode object,
     String jobId,
     Boolean batchingRequested
 ) {
